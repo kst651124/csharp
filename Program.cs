e3e95d2f55df2f33edf302e9e22b9f35745db7aa
kst651124 <kst651124@gmail.com>
2025-04-21 22:28:51 +0900

    all

--- Program.cs
+++ Program.cs
@@ -3,5 +3,19 @@ class Program{
 
     static void Main(){
         Console.WriteLine("아마도 우리가 제일 잘나가");
+        Money mainBank = new Money("신한은행", 80000000);
+        Console.WriteLine(String.Format("{0:N0}",mainBank.Amount));
+
+    }
+    public struct Money {
+        public string Bank { get; private set;}
+        public decimal Amount { get; private set;}
+
+        public Money(string bank, decimal amount)
+        {
+            this.Bank = bank;
+            this.Amount = amount;
+        }
+
     }
 }
